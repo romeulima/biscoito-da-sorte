766d362d697f95d5ee83f0b1dde306f5a4d55a5d
Romeu Lima <127458464+romeulima@users.noreply.github.com>
2024-02-25 21:50:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,40 @@ const button2 = document.querySelector('.button2')
 const screen1 = document.querySelector('.screen1')
 const screen2 = document.querySelector('.screen2')
 const card = document.querySelector('.card')
+const message = card.querySelector('p')
+const phrases = [
+"Se você não puder ser o Batman, seja o melhor Robin que conseguir.",
+"Às vezes, ser forte é reconhecer que precisamos de ajuda.",
+"A humildade é um tipo de inteligência.",
+"Só existe uma verdade, e ela foi sabiamente chamada de amor."
+]
+let index = 1
 
-button1.addEventListener('click', () => {
+button1.addEventListener('click', showPhrase)
+button2.addEventListener('click', resetPhrase)
+
+function showPhrase(){
     toggleScreen()
-    card.style.animation = "appear 0.7s cubic-bezier(0.390, 0.575, 0.565, 1.000)"
-})
+    animation()
+}
 
-button2.addEventListener('click', toggleScreen)
+function resetPhrase(){
+    if(index < phrases.length){
+        message.innerText = phrases[index]
+    }else{
+        index = 0
+        message.innerText = phrases[index]
+    }
+    index++
+    
+    toggleScreen()
+}
 
 function toggleScreen(){
     screen1.classList.toggle('hide')
     screen2.classList.toggle('hide')
+}
+
+function animation(){
+    card.style.animation = "appear 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000)"
 }
